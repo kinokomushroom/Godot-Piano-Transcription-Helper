5e00b5c86f176ad70350920d7ad6c09d0fbe0d48
kinokomushroom <kinokojoe@gmail.com>
2021-07-13 05:37:35 +0900

    Added filtering mode
    Added option that makes only the peak frequencies light up. Also, user can now control the minimum dB displayed.

--- Main.gd
+++ Main.gd
@@ -4,13 +4,20 @@ var screen_size: Vector2
 
 var key_number: int = 88
 var lowest_frequency: float = 27.5
-var magnitude_bar_width: float = 0.4
 var magnitude_db_array = []
-var averaging_frames: int = 4
 var smooth: bool = true
+var averaging_frames: int = 4
+var filter: bool = false
+var filtered_magnitude_array = []
+var filter_max_difference: float = 0.1
+var filter_change_speed: float = 0.08
 var lowest_magnitude: float = -60.0
 var magnitude_decrease_speed: float = 1.5 * lowest_magnitude
+
+var magnitude_bar_width: float = 0.4
 export var gradient: Gradient
+
+var key_white_array = [] # stores information of key color (true if white)
 var piano_bar_height: float = 0.15
 var piano_bar_width: float = 0.9
 onready var bar_screen_height: float = 1.0 - piano_bar_height
@@ -94,6 +101,21 @@ func analyze_frequencies():
 		magnitude_db = max(magnitude_db_array[key_index][averaging_frames - 2] + magnitude_decrease_speed * delta, magnitude_db)
 		magnitude_db = max(lowest_magnitude, magnitude_db)
 		magnitude_db_array[key_index][averaging_frames - 1] = magnitude_db
+	
+	for key_index in range(1, key_number - 1):
+		var center_value: float = magnitude_db_array[key_index][averaging_frames - 1]
+		var left_value: float = magnitude_db_array[key_index - 1][averaging_frames - 1]
+		var right_value: float = magnitude_db_array[key_index + 1][averaging_frames - 1]
+		if center_value >= left_value - filter_max_difference and center_value >= right_value - filter_max_difference:
+			if smooth:
+				filtered_magnitude_array[key_index] = min(1.0, filtered_magnitude_array[key_index] + filter_change_speed)
+			else:
+				filtered_magnitude_array[key_index] = 1.0
+		else:
+			if smooth:
+				filtered_magnitude_array[key_index] = max(0.0, filtered_magnitude_array[key_index] - filter_change_speed)
+			else:
+				filtered_magnitude_array[key_index] = 0.0
 
 func draw_magnitude(key_index: int):
 	var magnitude: float = magnitude_db_array[key_index][averaging_frames - 1]
@@ -106,18 +128,19 @@ func draw_magnitude(key_index: int):
 	if height_uniform < 0.5:
 		color = color.darkened(map_range(height_uniform, 0.0, 0.5, 0.9, 0.0))
 	else:
-		color = color.lightened(map_range(height_uniform, 0.5, 0.9, 0.0, 1.0))
+		color = color.lightened(map_range(height_uniform, 0.5, 1.0, 0.0, 1.0))
+	if filter:
+		color = color.darkened(1.0 - map_range(filtered_magnitude_array[key_index], 0.0, 1.0, 0.2, 1.0))
 	draw_rect(Rect2(x_position - width / 2, (screen_size.y - height) * bar_screen_height, width, height * bar_screen_height), color)
 
 func draw_key(key_index: int):
 	var x_position: float = screen_size.x / float(key_number) * (key_index + 0.5)
 	var width: float = screen_size.x / float(key_number) * piano_bar_width
 	var height: float = screen_size.y * piano_bar_height
-	var color: Color = white_key_color
-	for black_position in [1, 4, 6, 9, 11]:
-		if (key_index + 12 - black_position) % 12 == 0:
-			color = black_key_color
-	draw_rect(Rect2(x_position - width / 2, screen_size.y - height, width, height), color)
+	if key_white_array[key_index]:
+		draw_rect(Rect2(x_position - width / 2, screen_size.y - height, width, height), white_key_color)
+	else:
+		draw_rect(Rect2(x_position - width / 2, screen_size.y - height, width, height * 0.8), black_key_color)
 
 func _draw():
 	draw_rect(Rect2(0, 0, screen_size.x, screen_size.y), Color.black)
@@ -130,13 +153,23 @@ func viewport_size_changed():
 	$Control.rect_size = screen_size
 
 func _ready():
+	# make viewport resize detectable
 	get_tree().get_root().connect("size_changed", self, "viewport_size_changed")
 	viewport_size_changed()
+	
+	# initialize arrays
 	for key_index in range(0, key_number):
 		var history = []
 		for frame in averaging_frames:
 			history.append(lowest_magnitude)
 		magnitude_db_array.append(history)
+		
+		filtered_magnitude_array.append(0)
+		
+		key_white_array.append(true)
+		for black_position in [1, 4, 6, 9, 11]:
+			if (key_index + 12 - black_position) % 12 == 0:
+				key_white_array[key_index] = false
 
 func _process(_delta):
 	delta = _delta
@@ -220,3 +253,15 @@ func _on_smooth_toggled(button_pressed):
 	else:
 		smooth = false
 		$Control/smooth.text = "smoothing off"
+
+func _on_min_dB_value_changed(value):
+	lowest_magnitude = value
+	magnitude_decrease_speed = 1.5 * lowest_magnitude
+
+func _on_filter_toggled(button_pressed):
+	if button_pressed:
+		filter = true
+		$Control/filter.text = "filtering on"
+	else:
+		filter = false
+		$Control/filter.text = "filtering off"
